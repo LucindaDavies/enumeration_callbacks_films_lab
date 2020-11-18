79b17930941a847b5ccbe9f95fc4f3abe521371c
Lucinda Davies <lucindagdavies@gmail.com>
2020-11-18 14:10:40 +0000

    finished extensions

---
+++ lab_enumeration_callbacks_films_start/models/cinema.js
@@ -0,0 +1,33 @@
+const Cinema = function (films) {
+  this.films = films;
+};
+
+Cinema.prototype.listFilmTitles = function(){
+   return this.films.map(film => film.title);
+}
+
+Cinema.prototype.findByTitle = function(title){
+  return this.films.find(film => film.title === title)
+}
+
+Cinema.prototype.filterByGenre = function (genre){
+  return this.films.filter(film => film.genre === genre)
+}
+
+Cinema.prototype.isYearPresent = function (year) {
+  return this.films.some(film => film.year === year)
+}
+
+Cinema.prototype.areFilmsLongerThan = function (length) {
+  return this.films.every(film => film.length > length)
+}
+
+Cinema.prototype.totalRunningTime = function () {
+  return this.films.reduce((runningTime, film) => runningTime + film.length, 0)
+}
+
+Cinema.prototype.filmsByProperty = function (property, value) {
+  return this.films.filter(film => film[property] === value)
+}
+
+module.exports = Cinema;
